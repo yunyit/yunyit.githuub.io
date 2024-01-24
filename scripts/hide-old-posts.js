0411bcb3979831713ab1cc95b979d24d8570dd8a
“Yunyi” <winifred.tang.0820@gmail.com>
2024-01-24 23:57:40 +0000

    hexo-ing

---
+++ scripts/hide-old-posts.js
@@ -0,0 +1,7 @@
+hexo.config.hide_posts.acl_function_per_post = function (post, generatorName) {
+    if (post.date.year() < 2016 && post.hidden !== false) {
+      return false;
+    }
+  
+    return true;
+  };
